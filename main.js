803e4e9f6ae2cb685e146a8acc8fa45444069344
jlsajfj <jlsajfj@hotmail.com>
2016-12-19 21:26:25 -0400

    Implemented basic code and redid index layout

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-
+function cat(){
+	var change = document.getElementById('cat');
+	$.getJSON("http://random.cat/meow").then(function(data) {
+		console.log(data.file);
+		change.innerHTML="<img src=\""+data.file+"\">";
+	});
+}
