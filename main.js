c9f36ee606b43520bbccd5ec6f342d030578b355
jlsajfj <jlsajfj@hotmail.com>
2016-12-19 21:57:07 -0400

    Trying to make script safe

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function cat(){
 	var change = document.getElementById('cat');
-	$.getJSON("http://random.cat/meow").then(function(data) {
+	$.getJSON("https://random.cat/meow").then(function(data) {
 		console.log(data.file);
 		change.innerHTML="<img src=\""+data.file+"\">";
 	});
