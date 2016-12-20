85b101934cc8087e864fe191978a0ef1cc28844e
jlsajfj <jlsajfj@hotmail.com>
2016-12-19 22:10:53 -0400

    Trying to secure it

--- main.js
+++ main.js
@@ -1,7 +1,69 @@
 function cat(){
 	var change = document.getElementById('cat');
-	$.getJSON("http://random.cat/meow").then(function(data) {
+	/*var xhr = createCORSRequest('GET', 'http://random.cat/meow');
+	if (!xhr) {
+		throw new Error('CORS not supported');
+	}else{
+		xhr.send();
+	}*/
+	/*$.getJSON("http://random.cat/meow").then(function(data) {
 		console.log(data.file);
 		change.innerHTML="<img src=\""+data.file+"\">";
+	});*/
+	$.ajax({
+	  type: 'GET',
+	  url: 'http://random.cat/meow',
+	  contentType: 'text/plain',
+
+	  xhrFields: {
+		// The 'xhrFields' property sets additional fields on the XMLHttpRequest.
+		// This can be used to set the 'withCredentials' property.
+		// Set the value to 'true' if you'd like to pass cookies to the server.
+		// If this is enabled, your server must respond with the header
+		// 'Access-Control-Allow-Credentials: true'.
+		withCredentials: false
+	  },
+
+	  headers: {
+		// Set any custom headers here.
+		// If you set any non-simple headers, your server must include these
+		// headers in the 'Access-Control-Allow-Headers' response header.
+	  },
+
+	  success: function(data) {
+		  console.log(data.file);
+		  change.innerHTML="<img src=\""+data.file+"\">";
+	  },
+
+	  error: function() {
+		// Here's where you handle an error response.
+		// Note that if the error was due to a CORS issue,
+		// this function will still fire, but there won't be any additional
+		// information about the error.
+	  }
 	});
+}
+
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+
+    // Check if the XMLHttpRequest object has a "withCredentials" property.
+    // "withCredentials" only exists on XMLHTTPRequest2 objects.
+    xhr.open(method, url, true);
+
+  } else if (typeof XDomainRequest != "undefined") {
+
+    // Otherwise, check if XDomainRequest.
+    // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+
+  } else {
+
+    // Otherwise, CORS is not supported by the browser.
+    xhr = null;
+
+  }
+  return xhr;
 }
